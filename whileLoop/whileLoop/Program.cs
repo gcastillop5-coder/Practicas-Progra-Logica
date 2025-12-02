4dee255ad5504e79e57642f4d9352f007386b5f4
gcastillop5-coder <gcastillop5@miumg.edu.gt>
2025-12-01 21:02:33 -0600

    Primer commit: Estructura inicial proyectoe

---
+++ whileLoop/whileLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        // 1. Variable para guardar la suma total (La alcancía)
+        int total = 0;
+
+        // 2. Variable para leer lo que escribe el usuario. 
+        // La iniciamos en -1 solo para garantizar que entre al bucle la primera vez.
+        int venta = -1; 
+
+        Console.WriteLine("Bienvenido al sistema de ventas.");
+
+        // 3. El bucle: Mientras venta sea DISTINTO (!=) de 0...
+        while (venta != 0) {
+            Console.Write("Ingresa monto de venta (0 para salir): ");
+            
+            // A. Lee el número y guárdalo en la variable 'venta'
+            venta = int.Parse(Console.ReadLine());
+            
+            // B. Suma esa venta al total (El acumulador)
+            total = total + venta;
+        }
+
+        // 4. Al final, mostramos el resultado
+        Console.WriteLine("El total de ventas del día es: " + total);
+    }
+}
