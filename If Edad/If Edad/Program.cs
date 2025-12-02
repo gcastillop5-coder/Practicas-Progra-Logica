d1aa014d14d16df902f4a70474b5fe294561d40f
gcastillop5@miumg.edu.gt <gersonemanuel.castillopalencia@cdk.com>
2025-12-01 23:03:34 -0600

    Importanto proyecto If Edad al repositorio

---
+++ If Edad/If Edad/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+
+namespace Program
+{
+    class Edad
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Age");
+
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad < 12)
+            {
+                Console.WriteLine("You are a kid");
+            }
+            else if (edad < 17)
+            {
+                Console.WriteLine("You are a teenager");
+            }
+
+            else { Console.WriteLine("You are an adult"); }
+
+            
+        }
+    }
+}
